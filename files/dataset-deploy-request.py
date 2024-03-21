31eafd50a63d04c282c87ed0b7c32e7416e51c2d
Felix <felix.chung@felixchung.net>
2024-03-21 18:33:59 +0800

    minor fix

--- files/dataset-deploy-request.py
+++ files/dataset-deploy-request.py
@@ -74,7 +74,7 @@ def submitTicket():
         "group": "Users",
         "customer": "felix.chung@felixchung.org",
         "approved": False,
-        "datasetname": datasetPath
+        "datasetname": datasetPath,
         "article": {
            "subject": "Request dataset deployment",
            "body": f"Request dataset {datasetPath} production deployment. Please review the attached data mapping",
